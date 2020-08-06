f48d2f939c031316d69c7241336fbb8c3e8e4e14
andre12001 <andredavega123@gmail.com>
2020-08-06 09:14:35 +0700

    Template ChattApp Home Page Simple

---
+++ assets/js/ChattApp.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * ChattApp
+ *
+ * never change this ChattApp.js file because this is the default template
+ * because it can make you difficult in updating this site
+ *
+ */
+
+"use strict";
